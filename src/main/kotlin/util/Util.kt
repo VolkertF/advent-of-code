10da24af1b94a7db973f2b88b244ecf4aad02e91
Fabian Volkert <fabianvolkert1994@gmail.com>
2023-12-05 20:06:01 +0100

    add time measuring and averaging function

--- src/main/kotlin/util/Util.kt
+++ src/main/kotlin/util/Util.kt
@@ -1,6 +1,8 @@
 package util
 
 import java.io.BufferedReader
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.nanoseconds
 
 fun getResourceBufferedReader(path: String): BufferedReader? =
     {}.javaClass.getResourceAsStream("/$path")?.bufferedReader()
@@ -10,3 +12,12 @@ fun bufferedReaderTo2DStringList(reader: BufferedReader?): List<List<Char>> =
 fun resourceToStringList(path: String): List<String> = bufferedReaderToStringList(getResourceBufferedReader(path))
 fun resourceTo2DStringList(path: String): List<List<Char>> =
     bufferedReaderTo2DStringList(getResourceBufferedReader(path))
+fun getAverageExecutionTime(repeat: Int, fn: () -> Unit): Duration {
+    var average = 0.0
+    repeat(repeat) {
+        val currentTime = System.nanoTime()
+        fn()
+        average += (((System.nanoTime() - currentTime) - average) / (it + 1.0))
+    }
+    return average.nanoseconds
+}
