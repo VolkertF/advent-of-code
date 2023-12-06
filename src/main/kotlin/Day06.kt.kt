c2e4030b15f2968a638b2529f7ce836384a9ade4
Fabian Volkert <fabian.volkert@haufe-lexware.com>
2023-12-06 09:02:44 +0100

    feat: Add Day 6 solution

--- src/main/kotlin/Day06.kt.kt
+++ src/main/kotlin/Day06.kt.kt
@@ -11,11 +11,13 @@ class Day06 {
         }
 
 
-
         fun part02(lines: List<String>): Int {
             val times = lines[0].split(":")[1].trim()
-                .replace("\\s+".toRegex(),"").toLong()
-            val distances = lines[1].split(":")[1].trim().replace("\\s+".toRegex(),"").toLong()
+                .replace("\\s+".toRegex(), "")
+                .toLong()
+            val distances = lines[1].split(":")[1].trim()
+                .replace("\\s+".toRegex(), "")
+                .toLong()
             return reduceTimesToProductOfWinningOptions(listOf(times), listOf(distances))
         }
 
@@ -24,13 +26,9 @@ class Day06 {
 }
 
 fun reduceTimesToProductOfWinningOptions(times: List<Long>, distances: List<Long>): Int =
-    times.map { maxTime ->
+    times.mapIndexed { index, maxTime ->
         0.rangeTo(maxTime)
-            .map { buttonHoldingTime ->
-                (maxTime - buttonHoldingTime) * buttonHoldingTime
+            .count { buttonHoldingTime ->
+                (maxTime - buttonHoldingTime) * buttonHoldingTime > distances[index]
             }
-    }
-        .mapIndexed { index, myTimes ->
-            myTimes.count { it > distances[index] }
-        }
-        .reduce { acc, value -> acc * value }
+    }.reduce { acc, value -> acc * value }
